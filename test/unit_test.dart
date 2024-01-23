4f6ae63c4ae6bb9241167c32ce40d9017e335ab5
Gen Ichino <zenoplex@gmail.com>
2024-01-23 23:08:31 +0900

    test: Add unit test sample

---
+++ test/unit_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_sandbox/main.dart';
+
+void main() {
+  test('Is even', () {
+    bool result = isEven(12);
+    expect(result, true);
+
+    result = isEven(13);
+    expect(result, false);
+  });
+}
