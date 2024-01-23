91c381a552652b1e50a99767ae1010b359d31b80
Gen Ichino <zenoplex@gmail.com>
2024-01-23 23:11:47 +0900

    test: Use grouping

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -2,13 +2,15 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_sandbox/main.dart';
 
 void main() {
-  test('Is even', () {
-    bool result = isEven(12);
-    expect(result, true);
-  });
+  group('isEven group', () {
+    test('Is even', () {
+      bool result = isEven(12);
+      expect(result, true);
+    });
 
-  test('Is odd', () {
-    bool result = isEven(13);
-    expect(result, false);
+    test('Is odd', () {
+      bool result = isEven(13);
+      expect(result, false);
+    });
   });
 }
