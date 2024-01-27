10d633d07a7d9253143347739b3746ba9165ae78
Gen Ichino <zenoplex@gmail.com>
2024-01-27 23:39:56 +0900

    feat: Add text column in text_layout

--- lib/text_layout.dart
+++ lib/text_layout.dart
@@ -5,6 +5,18 @@ class TextLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Hello World!',
+          style: TextStyle(fontSize: 30),
+        ),
+        Text('Text can wrap without issue',
+            style: Theme.of(context).textTheme.titleLarge),
+        const Text(
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut fermentum ante eu sapien tincidunt, eu pulvinar eros pharetra. Vestibulum non efficitur magna, vel interdum dui. Donec consectetur lorem purus, sit amet consequat sapien dignissim vel. ')
+      ],
+    );
   }
 }
