1bb843548a6df78ca71aaf7be8e32260a9f2731e
Gen Ichino <zenoplex@gmail.com>
2024-02-02 01:01:20 +0900

    feat: Switch to FlexScreen in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_sandbox/profile_screen.dart';
+import 'package:flutter_sandbox/flex_screen.dart';
+// import 'package:flutter_sandbox/profile_screen.dart';
 
 void main() {
   runApp(const StaticApp());
@@ -13,6 +14,7 @@ class StaticApp extends StatelessWidget {
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(primarySwatch: Colors.blue),
-        home: const ProfileScreen());
+        // home: const ProfileScreen());
+        home: const FlexScreen());
   }
 }
