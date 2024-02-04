a2ef6240596f71a3447f1337575bfbbfd08af93b
Gen Ichino <zenoplex@gmail.com>
2024-02-05 00:53:09 +0900

    feat: Add stopwatch functionality to the app

--- lib/stopwatch.dart
+++ lib/stopwatch.dart
@@ -9,14 +9,33 @@ class StopWatch extends StatefulWidget {
 }
 
 class _StopWatchState extends State<StopWatch> {
+  int seconds = 0;
+  late Timer timer;
+
+  @override
+  void initState() {
+    super.initState();
+    timer = Timer.periodic(const Duration(seconds: 1), _onTick);
+  }
+
+  void _onTick(Timer timer) {
+    if (!mounted) return;
+
+    setState(() {
+      seconds++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text('Stopwatch')),
         body: SafeArea(
           child: Center(
-              child: Text('0 seconds',
+              child: Text('$seconds ${_secondsText()}',
                   style: Theme.of(context).textTheme.headlineSmall)),
         ));
   }
+
+  String _secondsText() => seconds <= 1 ? 'second' : 'seconds';
 }
