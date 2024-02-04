019cbf560f1bd015d23679d1886eec23d8f8d63f
Gen Ichino <zenoplex@gmail.com>
2024-02-04 23:57:57 +0900

    feat: Add stopwatch app boilerplate

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,15 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_sandbox/flex_screen.dart';
-// import 'package:flutter_sandbox/profile_screen.dart';
-// import 'package:flutter_sandbox/deep_tree.dart';
-import 'package:flutter_sandbox/e_commerce_screen.dart';
+import 'package:flutter_sandbox/stopwatch.dart';
 
 void main() {
-  runApp(const StaticApp());
+  runApp(const StopWatchApp());
 }
 
-class StaticApp extends StatelessWidget {
-  const StaticApp({super.key});
+class StopWatchApp extends StatelessWidget {
+  const StopWatchApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        appBarTheme: _buildAppBarTheme(),
-        brightness: Brightness.light,
-      ),
-      darkTheme: ThemeData(
-        primarySwatch: Colors.green,
-        appBarTheme: _buildAppBarTheme(),
-        brightness: Brightness.dark,
-      ),
-      themeMode: ThemeMode.system,
-      home: const ECommerceScreen(),
-      // home: const DeepTree()
-      // home: const ProfileScreen()
-      // home: const FlexScreen());
-    );
-  }
-
-  AppBarTheme _buildAppBarTheme() {
-    return AppBarTheme(
-      elevation: 10,
-      iconTheme: const IconThemeData(
-        color: Colors.white,
-      ),
-      titleTextStyle: const TextTheme(
-          titleLarge: TextStyle(
-        fontSize: 24,
-        fontFamily: 'LeckerliOne',
-      )).titleLarge,
-      backgroundColor: Colors.green,
-    );
+    return const StopWatch();
   }
 }
---
+++ lib/stopwatch.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class StopWatch extends StatefulWidget {
+  const StopWatch({super.key});
+
+  @override
+  State<StopWatch> createState() => _StopWatchState();
+}
+
+class _StopWatchState extends State<StopWatch> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
