3ae23978585690e91eead10b2db86152d2466c87
Gen Ichino <zenoplex@gmail.com>
2024-02-12 21:24:27 +0900

    feat: Update main.dart with new app name and theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_sandbox/login_screen.dart';
 
 void main() {
-  runApp(const StopWatchApp());
+  runApp(const MasterPlanApp());
 }
 
-class StopWatchApp extends StatelessWidget {
-  const StopWatchApp({super.key});
+class MasterPlanApp extends StatelessWidget {
+  const MasterPlanApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.blue,
-            brightness: Brightness.light,
-          )),
-      home: const LoginScreen(),
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+          brightness: Brightness.dark,
+        ),
+      ),
+      home: const Placeholder(),
     );
   }
 }
