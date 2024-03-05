efff41772b2b1dfac65f5ba3fbd3e57550cf51e4
Gen Ichino <zenoplex@gmail.com>
2024-03-06 01:09:19 +0900

    feat: Add HTTP request functionality to FutureScreen

--- lib/views/future_screen.dart
+++ lib/views/future_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 class FutureScreen extends StatefulWidget {
   const FutureScreen({super.key});
@@ -19,7 +22,17 @@ class _FutureScreenState extends State<FutureScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Spacer(),
-            ElevatedButton(onPressed: () {}, child: const Text('Go!')),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {});
+                  getData().then((value) {
+                    result = value.body.toString().substring(0, 450);
+                    setState(() {});
+                  }).catchError((_) {
+                    result = 'An error occurred';
+                  });
+                },
+                child: const Text('Go!')),
             const Spacer(),
             Text(result),
             const Spacer(),
@@ -29,5 +42,12 @@ class _FutureScreenState extends State<FutureScreen> {
         ),
       ),
     );
+  }
+
+  Future<Response> getData() async {
+    const authority = 'www.googleapis.com';
+    const path = '/books/v1/volumes/junbDwAAQBAJ';
+    Uri url = Uri.https(authority, path);
+    return http.get(url);
   }
 }
