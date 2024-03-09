6a38159688996e5cb61f1d54b180b226e96b00a8
Gen Ichino <zenoplex@gmail.com>
2024-03-09 21:35:41 +0900

    feat: Add pizza list from JSON file

--- lib/views/json_home_screen.dart
+++ lib/views/json_home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_sandbox/models/pizza.dart';
 
 class JsonHomePage extends StatefulWidget {
   const JsonHomePage({super.key});
@@ -8,13 +11,41 @@ class JsonHomePage extends StatefulWidget {
 }
 
 class _JsonHomePageState extends State<JsonHomePage> {
+  List _pizzas = [];
+
+  @override
+  void initState() {
+    super.initState();
+    readJsonFile().then((value) {
+      setState(() {
+        _pizzas = value;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('JSON'),
       ),
-      body: Container(),
+      body: ListView.builder(
+        itemCount: _pizzas.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text('$index'),
+          );
+        },
+      ),
     );
+  }
+
+  Future<List<Pizza>> readJsonFile() async {
+    String data =
+        await DefaultAssetBundle.of(context).loadString('assets/pizzas.json');
+
+    List list = jsonDecode(data);
+    List<Pizza> pizzas = list.map((item) => Pizza.fromJson(item)).toList();
+    return pizzas;
   }
 }
