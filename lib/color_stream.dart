268bd276118b12c2401a99aa901fd5b8af43fe46
Gen Ichino <zenoplex@gmail.com>
2024-03-16 21:49:22 +0900

    feat: Add ColorStream class to generate a stream of colors

---
+++ lib/color_stream.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ColorStream {
+  final List<Color> colors = [
+    Colors.blueGrey,
+    Colors.amber,
+    Colors.deepPurple,
+    Colors.lightBlue,
+    Colors.teal,
+  ];
+
+  Stream<Color> getColors() async* {
+    yield* Stream.periodic(const Duration(seconds: 1), (int t) {
+      int index = t % colors.length;
+      return colors[index];
+    });
+  }
+}
