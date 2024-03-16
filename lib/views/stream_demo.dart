1c506f6f41a12d1a964e923b6abb2f2f2bba42bc
Gen Ichino <zenoplex@gmail.com>
2024-03-16 22:33:54 +0900

    feat: Add number stream and random number button

--- lib/views/stream_demo.dart
+++ lib/views/stream_demo.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_sandbox/color_stream.dart';
+import 'package:flutter_sandbox/streams.dart';
 
 class StreamHomePage extends StatefulWidget {
   const StreamHomePage({super.key});
@@ -11,12 +14,23 @@ class StreamHomePage extends StatefulWidget {
 class _StreamHomePageState extends State<StreamHomePage> {
   Color bgColor = Colors.blueGrey;
   final ColorStream colorStream = ColorStream();
+  int lastNumber = 0;
+  final NumberStream numberStream = NumberStream();
+  // final StreamController<int> numberStreamController = StreamController<int>();
+  late StreamController numberStreamController;
 
   @override
   void initState() {
-    super.initState();
+    numberStreamController = numberStream.controller;
+    final Stream stream = numberStreamController.stream;
+    stream.listen((event) {
+      setState(() {
+        lastNumber = event;
+      });
+    });
 
     changeColor();
+    super.initState();
   }
 
   @override
@@ -27,8 +41,29 @@ class _StreamHomePageState extends State<StreamHomePage> {
       ),
       body: Container(
         decoration: BoxDecoration(color: bgColor),
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(lastNumber.toString()),
+              ElevatedButton(
+                  onPressed: () {
+                    addRandomNumber();
+                  },
+                  child: const Text('New Random Number'))
+            ],
+          ),
+        ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    numberStreamController.close();
+    super.dispose();
   }
 
   void changeColor() async {
@@ -37,5 +72,11 @@ class _StreamHomePageState extends State<StreamHomePage> {
         bgColor = eventColor;
       });
     }
+  }
+
+  void addRandomNumber() {
+    Random random = Random();
+    final int num = random.nextInt(10);
+    numberStream.addNumberToSink(num);
   }
 }
