0e89bcd6b269f8be5973b80005770ae33329324d
Gen Ichino <zenoplex@gmail.com>
2024-03-18 00:45:04 +0900

    feat: Update main.dart and add RandomBlocScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_sandbox/views/stream_builder_app.dart';
+import 'package:flutter_sandbox/views/random_bloc_screen.dart';
 
 void main() {
   runApp(const MasterPlanApp());
@@ -19,7 +19,7 @@ class MasterPlanApp extends StatelessWidget {
           brightness: Brightness.dark,
         ),
       ),
-      home: const StreamBuilderApp(),
+      home: const RandomBlocScreen(),
     );
   }
 }
---
+++ lib/views/random_bloc_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sandbox/random_bloc.dart';
+
+class RandomBlocScreen extends StatefulWidget {
+  const RandomBlocScreen({super.key});
+
+  @override
+  State<RandomBlocScreen> createState() => _RandomBlocScreenState();
+}
+
+class _RandomBlocScreenState extends State<RandomBlocScreen> {
+  final _bloc = RandomNumberBloc();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Random Number')),
+      body: StreamBuilder<int>(
+        stream: _bloc.randomNumber,
+        builder: (context, snapshot) {
+          return Text(
+            'Random Number: ${snapshot.data.toString()}',
+            style: const TextStyle(fontSize: 30),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _bloc.generateRandom.add(null);
+        },
+        child: const Icon(Icons.refresh),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _bloc.dispose();
+    super.dispose();
+  }
+}
