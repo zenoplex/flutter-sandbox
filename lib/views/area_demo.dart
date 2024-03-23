dd3f1ef7ddbeaf6b5105eb10cd5ba41bd1da3a58
Gen Ichino <zenoplex@gmail.com>
2024-03-24 00:58:55 +0900

    feat: Update main.dart and add area_demo.dart

---
+++ lib/views/area_demo.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class AreaApp extends StatefulWidget {
+  const AreaApp({super.key});
+
+  @override
+  State<AreaApp> createState() => _AreaAppState();
+}
+
+class _AreaAppState extends State<AreaApp> {
+  final TextEditingController widthController = TextEditingController();
+  final TextEditingController heightController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Package App'),
+      ),
+      body: Column(
+        children: [
+          AppTextField(widthController, 'Width'),
+          AppTextField(heightController, 'Height'),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    widthController.dispose();
+    heightController.dispose();
+    super.dispose();
+  }
+}
+
+class AppTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String label;
+
+  const AppTextField(this.controller, this.label, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(hintText: label),
+      ),
+    );
+  }
+}
