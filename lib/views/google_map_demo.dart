83802950829da2932e653888d7dfffd544561ca3
Gen Ichino <zenoplex@gmail.com>
2024-03-30 10:38:05 +0900

    feat: Use location service

--- lib/views/google_map_demo.dart
+++ lib/views/google_map_demo.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class GoogleMapApp extends StatefulWidget {
   const GoogleMapApp({super.key});
@@ -13,10 +14,39 @@ class _GoogleMapAppState extends State<GoogleMapApp> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Google Map')),
-      body: const GoogleMap(
-        initialCameraPosition:
-            CameraPosition(target: LatLng(35.652832, 139.839478), zoom: 12),
+      body: FutureBuilder(
+        future: getUserLocation(),
+        builder: (BuildContext context, AsyncSnapshot<LatLng?> snapshot) {
+          if (snapshot.hasData) {
+            return GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: snapshot.data!,
+                zoom: 12,
+              ),
+            );
+          }
+
+          return Container();
+        },
       ),
     );
+  }
+
+  Future<LatLng?> getUserLocation() async {
+    final Location location = Location();
+    final PermissionStatus hasPermission = await location.hasPermission();
+    final bool isServiceEnabled = await location.serviceEnabled();
+
+    if (hasPermission == PermissionStatus.granted && isServiceEnabled) {
+      final LocationData locationData = await location.getLocation();
+      final double? latitude = locationData.latitude;
+      final double? longitude = locationData.longitude;
+
+      if (latitude == null || longitude == null) {
+        return null;
+      }
+      return LatLng(latitude, longitude);
+    }
+    return null;
   }
 }
