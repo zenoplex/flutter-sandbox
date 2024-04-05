4db67d9491ffc50eff431f8ab5312b6b683a14f3
Gen Ichino <zenoplex@gmail.com>
2024-04-06 03:12:56 +0900

    test: Refactor area_test.dart to use a list of test cases for calculateAreaRect

--- packages/area/test/area_test.dart
+++ packages/area/test/area_test.dart
@@ -1,10 +1,20 @@
+import 'package:area/area.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+    final List<({String expected, ({double height, double width}) input})>
+        cases = [
+      (input: (width: 10.0, height: 10.0), expected: '100'),
+      (input: (width: 500.0, height: 100.0), expected: '50,000'),
+      (input: (width: 2.0, height: 2.0), expected: '4'),
+      (input: (width: 0, height: 2.0), expected: '0'),
+      (input: (width: -100, height: 100), expected: '-10,000'),
+    ];
+
+    for (final (:input, :expected) in cases) {
+      final (:width, :height) = input;
+      expect(calculateAreaRect(width, height), expected);
+    }
   });
 }
