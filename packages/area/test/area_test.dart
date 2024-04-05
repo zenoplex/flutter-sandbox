09a8476c0d43d5c84f0b633d8457ffb6f1580f14
Gen Ichino <zenoplex@gmail.com>
2024-04-06 03:16:56 +0900

    test: Refactor area_test.dart to use a list of test cases for calculateAreaRect and add calculateAreaTriangle test

--- packages/area/test/area_test.dart
+++ packages/area/test/area_test.dart
@@ -2,7 +2,7 @@ import 'package:area/area.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('adds one to input values', () {
+  test('calculateAreaRect', () {
     final List<({String expected, ({double height, double width}) input})>
         cases = [
       (input: (width: 10.0, height: 10.0), expected: '100'),
@@ -10,11 +10,29 @@ void main() {
       (input: (width: 2.0, height: 2.0), expected: '4'),
       (input: (width: 0, height: 2.0), expected: '0'),
       (input: (width: -100, height: 100), expected: '-10,000'),
+      (input: (width: 10.5, height: 20.21), expected: '212'),
     ];
 
     for (final (:input, :expected) in cases) {
       final (:width, :height) = input;
       expect(calculateAreaRect(width, height), expected);
+    }
+  });
+
+  test('calculateAreaTriangle', () {
+    final List<({String expected, ({double height, double width}) input})>
+        cases = [
+      (input: (width: 10.0, height: 10.0), expected: '50'),
+      (input: (width: 500.0, height: 100.0), expected: '25,000'),
+      (input: (width: 2.0, height: 2.0), expected: '2'),
+      (input: (width: 0, height: 2.0), expected: '0'),
+      (input: (width: -100, height: 100), expected: '-5,000'),
+      (input: (width: 10.5, height: 20.21), expected: '106'),
+    ];
+
+    for (final (:input, :expected) in cases) {
+      final (:width, :height) = input;
+      expect(calculateAreaTriangle(width, height), expected);
     }
   });
 }
