46ea897cca5340120dc7fa96e11ecc25dd392530
Gen Ichino <zenoplex@gmail.com>
2024-05-09 01:09:45 +0900

    feat: Add Google Identity Platform configuration for sign-in with email

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,3 +74,19 @@ resource "google_storage_bucket" "tfstate" {
     enabled = true
   }
 }
+
+resource "google_identity_platform_config" "default" {
+  provider = google-beta
+  project  = google_project.default.project_id
+
+  depends_on = [
+    google_project_service.default,
+  ]
+
+  sign_in {
+    email {
+      enabled           = true
+      password_required = false
+    }
+  }
+}
