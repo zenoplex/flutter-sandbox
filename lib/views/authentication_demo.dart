78677546c687e0a6b6cf0936d10f59660e7df206
Gen Ichino <zenoplex@gmail.com>
2024-05-18 23:57:36 +0900

    feat: Add PollScreen for voting functionality

--- lib/views/authentication_demo.dart
+++ lib/views/authentication_demo.dart
@@ -1,7 +1,55 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
+
+class PollScreen extends StatefulWidget {
+  const PollScreen({super.key});
+
+  @override
+  State<PollScreen> createState() => _PollScreenState();
+}
+
+class _PollScreenState extends State<PollScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        ElevatedButton(
+          onPressed: () {},
+          child: const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Icon(Icons.icecream),
+              Text('Ice-cream'),
+            ],
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {vote(true);},
+          child: const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Icon(Icons.local_pizza),
+              Text('Pizza'),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+/// Vote
+Future<void> vote(bool voteForPizza) async {
+  final FirebaseFirestore db = FirebaseFirestore.instance;
+  final CollectionReference collection = db.collection('poll');
+  final QuerySnapshot snapshot = await collection.get();
+
+  print(snapshot.docs[0].id);
+}
 
 class HappyScreen extends StatefulWidget {
   const HappyScreen({super.key});
@@ -13,18 +61,11 @@ class HappyScreen extends StatefulWidget {
 class _HappyScreenState extends State<HappyScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Happy Happy!'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text("I'm happy!"),
-          onPressed: () {
-            FirebaseAnalytics.instance.logEvent(name: 'Happy');
-          },
-        ),
-      ),
+    return ElevatedButton(
+      child: const Text("I'm happy!"),
+      onPressed: () {
+        FirebaseAnalytics.instance.logEvent(name: 'Happy');
+      },
     );
   }
 }
@@ -66,7 +107,14 @@ class AuthenticationDemo extends StatelessWidget {
             },
           );
         }
-        return const HappyScreen();
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Happy Happy!'),
+          ),
+          body: const Column(
+            children: [HappyScreen(), PollScreen()],
+          ),
+        );
       },
     );
   }
