a6dd5a8b5ba16b67db50ae35de29630ab6177448
Gen Ichino <zenoplex@gmail.com>
2024-05-27 22:31:12 +0900

    chore: Extract Google Identity Platform configuration and supported IDP config

--- terraform/main.tf
+++ terraform/main.tf
@@ -80,29 +80,4 @@ resource "google_storage_bucket" "tfstate" {
   }
 }
 
-resource "google_identity_platform_config" "default" {
-  provider = google-beta
-  project  = google_project.default.project_id
-
-  depends_on = [
-    google_project_service.default,
-  ]
-
-  sign_in {
-    email {
-      enabled           = true
-      password_required = false
-    }
-  }
-}
-
-resource "google_identity_platform_default_supported_idp_config" "idp_config" {
-  provider = google-beta
-  project  = google_project.default.project_id
-
-  enabled       = true
-  idp_id        = "google.com"
-  client_id     = var.google_client_id
-  client_secret = var.google_client_secret
-}
 
